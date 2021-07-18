91ba2eb1c2e7efa040951d6b12a983a2aacaa39c
Bochen Li <550447392@qq.com>
2021-07-18 11:40:14 +0800

    A little revision

--- SourceCode/Geometry.m
+++ SourceCode/Geometry.m
@@ -1,68 +1,10 @@
-% ----------------------------------------------------------------------- %
-%                                                                         %
-%                              0111000                                    %
-%                           100 1 100 001                                 %
-%                         10    1  1  1 00                                %
-%                        01  1  1  1      0                               %
-%                       0 1  1  1   1  1 1 0                              %
-%                       0   1   1   1  1  1 0                             %
-%                       0 1     1   1  1  1 0                             %
-%                       0 1  1  1   1  0  1 0                             %
-%                       0 1  1  1   0  1    0                             %
-%                       01 1        1  1 1 0                              %
-%                        0    0  1  0 1   0                               %
-%                         0         1    0                                %
-%                    10010 0 1101111110111                                %
-%                  10 1 1  1111111111 11 11                               %
-%                 0 1 1 1 11111111101011010111                            %
-%                01 11    11111111 1  1    1 110                          %
-%               011    1 1 111111110011  1 1 1 110                        %
-%               0   11 1 1 1 111      0  1 1 1   10                       %
-%               0 1   11  1  0         1 1 1 1 1 1 0                      %
-%               1  11 1 1   11          0  1 1 1 1 11                     %
-%                0     1 1  0           011  1 1 1 10                     %
-%                10 1   1  0             0  1 1 1  11                     %
-%                 10     01               01      10                      %
-%                   10001                   001 100                       %
-%                                             111                         %
-%                                                                         %
-%             ____                   _____                                %
-%            / __ \                 |  __ \                               %
-%           | |  | |_ __   ___ _ __ | |__) | __ ___  _ __                 %
-%           | |  | | '_ \ / _ \ '_ \|  ___/ '__/ _ \| '_ \                %
-%           | |__| | |_) |  __/ | | | |   | | | (_) | |_) |               %
-%            \____/| .__/ \___|_| |_|_|   |_|  \___/| .__/                %
-%                  | |                              | |                   %
-%                  |_|                              |_|                   %
-%                                                                         %
-%             An integrated rotor design and analysis tool.               %
-%                                                                         %
-%                                                                         %
-% Copyright (C) 2011, Brenden Epps.                                       %
-%                                                                         %
-% This program is free software; you can redistribute it and/or modify it %
-% under the terms of the GNU General Public License as published by the   %
-% Free Software Foundation.                                               %
-%                                                                         %
-% This program is distributed in the hope that it will be useful, but     %
-% WITHOUT ANY WARRANTY; without even the implied warranty of              %
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                    %
-% See the GNU General Public License for more details.                    %
-%                                                                         %
-% ----------------------------------------------------------------------- %
-
-
-% =========================================================================
-% =================================== Determine Propeller Geometry Function    
+%This function determines the geometry of the propeller.
+%It output the geometry as a 2D image, 3D image, and Rhino CAD file.
 %
-% This function determines the geometry of the propeller.  It outputs
-% the geometry as a 2D image, 3D image, and Rhino CAD file.
-%
-% Reference: 
-%   J.S. Carlton, "Marine Propellers & Propulsion", ch. 3, 1994.
-%
-%   Abbott, I. H., and Von Doenhoff, A. E.; Theory of Wing Sections. 
-%   Dover, 1959. 
+%Reference: 
+%[1]J.S. Carlton, "Marine Propellers & Propulsion", ch. 3, 1994.
+%[2]Abbott, I. H., and Von Doenhoff, A. E.; Theory of Wing Sections. 
+%Dover, 1959. 
 %
 % -------------------------------------------------------------------------
 % Input Variables:
